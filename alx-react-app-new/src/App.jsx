90cea12febcdcf7afce49225c7c5aca5d96208cc
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-01-31 05:09:45 +0200

    applying state and hooks

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,11 +4,13 @@ import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
 import UserProfile from './components/UserProfile.jsx'
+import Counter from './components/Counter.jsx'
 
 function App() {
   return (
     <>
       <Header />
+      <Counter />
       <MainContent />
       <WelcomeMessage />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" /> 
