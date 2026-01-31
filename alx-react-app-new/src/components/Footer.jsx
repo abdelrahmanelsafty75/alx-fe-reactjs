e5f61910ff5a013dc85b78e160822a4dc8a4d841
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-01-31 05:03:11 +0200

    Apply Inline CSS Styling to React Components

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer>
+    <footer style={{ backgroundColor: 'lightgray', textAlign: 'center', padding: '10px' }}>
   <p>© 2023 City Lovers</p>
     </footer>
   )
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function MainContent() {
   return (
-    <main>
+    <main style={{ padding: '20px', fontFamily: 'Arial, sans-serif',backgroundColor: '#f9f9f9' }}>
   <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   )
