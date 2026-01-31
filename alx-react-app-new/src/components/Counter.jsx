99c9b038bc91da6980c6ddf4cea6a772b76cae66
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-01-31 05:13:00 +0200

    Counter feature with basic styling

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import { useState } from 'react';
 function Counter() {
     const [count, setCount] = useState(0);
+    const btnStyle = {
+        margin: '5px',
+        padding: '10px',
+        backgroundColor: '#4CAF50',
+    };
   return (
-    <div>
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px',fontSize: '16px' }}>
     <p>Current Count: {count}</p>
-    <button onClick={() => setCount(count + 1)}>Increment</button>
-    <button onClick={() => setCount(count - 1)}>Decrement</button>
-    <button onClick={() => setCount(0)}>Reset</button>
+    <button style={btnStyle} onClick={() => setCount(count + 1)}>Increment</button>
+    <button style={btnStyle} onClick={() => setCount(count - 1)}>Decrement</button>
+    <button style={btnStyle} onClick={() => setCount(0)}>Reset</button>
     </div>
   )
 }
