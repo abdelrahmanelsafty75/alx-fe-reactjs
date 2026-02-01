43cc4505099b2b1e076c4fa47fdca9a07983ee59
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-02-01 04:11:54 +0200

    create and add Navbar component

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -4,11 +4,13 @@ import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import Services from './components/Services';
+import Navebar from './components/Navebar';
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 function App() {
 
   return (
     <BrowserRouter>
+      <Navebar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+function Navbar() {
+    const navStyle={
+        margin: '0 10px'
+    }
+  return (
+    <div>
+        <nav style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
+            <Link to="/" style={navStyle}>Home</Link>
+            <Link to="/about" style={navStyle}>About</Link>
+            <Link to="/services" style={navStyle}>Services</Link>
+            <Link to="/contact" style={navStyle}>Contact</Link>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
