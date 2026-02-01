a44eec4ddce73f94a4c772130755d94733c79eb0
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-02-01 04:07:02 +0200

    app routing

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
-
+import React from "react";
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Services from './components/Services';
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 function App() {
 
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
