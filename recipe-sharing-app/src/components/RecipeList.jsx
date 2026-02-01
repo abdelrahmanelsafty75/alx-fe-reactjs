77e229fc3df1a2b0ffc354f8d3bd81caf8c48c18
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-02-01 23:20:43 +0200

    components creation

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import useRecipeStore from '../store/useRecipeStore'
+import useRecipeStore from './useRecipeStore'
 function RecipeList() {
     const recipes = useRecipeStore((state) => state.recipes);
   return (
