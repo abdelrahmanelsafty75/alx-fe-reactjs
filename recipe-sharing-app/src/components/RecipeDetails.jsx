0964892ba9ae532e92e02ba9e1ef2cff48ec18a2
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-02-03 03:48:17 +0200

    adding favorite button

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -7,14 +7,31 @@ function RecipeDetails() {
     const { recipeID } = useParams();
     const recipe = useRecipeStore(state => state.recipes.find(r => r.id === Number(recipeID)));
 
+    const favorites = useRecipeStore(state => state.favorites);
+    const addFavorite = useRecipeStore(state => state.addFavorite);
+    const removeFavorite = useRecipeStore(state => state.removeFavorite);
+
+    
     if (!recipe) {
         return <div>Recipe not found</div>;
     }
+    
+    const isFavorite = favorites.includes(recipe?.id);
+
     return (
         <div>
             <h2>{recipe.title}</h2>
             <p>{recipe.description}</p>
             <hr />
+            <button onClick={() => {
+                if (isFavorite) {
+                    removeFavorite(recipe.id);
+                } else {
+                    addFavorite(recipe.id);
+                }
+            }}>
+                {isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
+            </button>
             <EditRecipeForm recipeID={recipe.id} />
             <DeleteRecipeButton recipeID={recipe.id} />
         </div>
