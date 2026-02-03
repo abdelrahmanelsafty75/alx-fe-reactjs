1e3ad670edc9de5f52f3ca12031d27612f21c1c8
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-02-03 02:21:46 +0200

    optimized with search listing

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -2,12 +2,16 @@ import useRecipeStore from './recipeStore';
 import { Link } from 'react-router-dom'; 
 const RecipeList = () => {
   const recipes = useRecipeStore(state => state.recipes);
+  const searchTerm = useRecipeStore(state => state.searchTerm);
+  const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
+
+  const displayRecipes = searchTerm ? filteredRecipes : recipes;
 
   return (
     <div>
-       {recipes.length === 0 ? <p>No recipes yet.</p> : null}
+       {recipes.length === 0 ? <h3>No recipes yet.</h3> : null}
        
-      {recipes.map(recipe => (
+      {displayRecipes.map(recipe => (
         <div key={recipe.id} style={{ border: '1px solid #ccc', margin: '10px', padding: '10px' }}>
           <h3>{recipe.title}</h3>
           <p>{recipe.description}</p>
