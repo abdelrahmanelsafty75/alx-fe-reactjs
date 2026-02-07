dfaa23f2b059c707407e878a44bb1b02c4fafce6
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-02-07 04:16:31 +0200

    advanced search api

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -4,3 +4,18 @@ export const fetchUserData = async (username) => {
   const response = await axios.get(`https://api.github.com/users/${username}`);
   return response.data;
 };
+
+export const fetchAdvancedSearchResults = async (username, location, minRepos, page = 1) => {
+  let query = username || '';
+
+  if (location) {
+    query += `+location:${location}`;
+  }
+
+  if (minRepos) {
+    query += `+repos:>${minRepos}`;
+  }
+
+  const response = await axios.get(`https://api.github.com/search/users?q=${query}&page=${page}`);
+  return response.data;
+};
