6dd44e8fcc924bdd9579fc2d9e55350101427ae7
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-02-12 04:50:17 +0200

    more transitions

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 sm:p-4 sm:max-w-xs md:max-w-sm md:p-8  mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 sm:p-4 sm:max-w-xs md:max-w-sm md:p-8  mx-auto my-20 rounded-lg shadow-lg  hover:shadow-xl">
       
-      <img className="md:w-36 md:h-36 sm:w-24 sm:h-24 rounded-full mx-auto" src="https://via.placeholder.com/150" alt="User" />
+      <img className="md:w-36 md:h-36 sm:w-24 sm:h-24 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out" src="https://via.placeholder.com/150" alt="User" />
       
-      <h1 className="md:text-xl sm:text-lg font-bold my-4 text-blue-800">John Doe</h1>
+      <h1 className="md:text-xl sm:text-lg font-bold my-4 text-blue-800 hover:text-blue-500">John Doe</h1>
       
       <p className="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
