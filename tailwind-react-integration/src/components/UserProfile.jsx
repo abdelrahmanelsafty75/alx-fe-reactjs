6a3d66c7ff6e5d9d0efb9a1ba2198ab3171facc4
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-02-12 04:45:46 +0200

    responsive design

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile() {
   return (
     <div className="bg-gray-100 sm:p-4 sm:max-w-xs md:max-w-sm md:p-8  mx-auto my-20 rounded-lg shadow-lg">
       
-      <img className="md:w-36 md:h-36 sm:w-26 sm:h-26 rounded-full mx-auto" src="https://via.placeholder.com/150" alt="User" />
+      <img className="md:w-36 md:h-36 sm:w-24 sm:h-24 rounded-full mx-auto" src="https://via.placeholder.com/150" alt="User" />
       
       <h1 className="md:text-xl sm:text-lg font-bold my-4 text-blue-800">John Doe</h1>
       
